323ac9c8644019b631b8cce57c86d64439fd3c68
Sczygelski <avamariescz@gmail.com>
2022-10-05 20:41:56 -0500

    adding associations

--- commerce/Develop/models/index.js
+++ commerce/Develop/models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsToMany(Category, {
+  foreignKey:
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey:
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
 
+})
 // Tags belongToMany Products (through ProductTag)
+ProductTag.belongsToMany(Product, {
+  
+})
 
 module.exports = {
   Product,
