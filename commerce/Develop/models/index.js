1c2a6cb424a83c612d819de21482f70e307d6dca
Sczygelski <avamariescz@gmail.com>
2022-10-10 19:32:55 -0500

    fixing index model

--- commerce/Develop/models/index.js
+++ commerce/Develop/models/index.js
@@ -6,19 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsToMany(Category, {
-  foreignKey:
+  foreignKey: 'category_id'
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey:
+  foreignKey: 'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
-ProductTag.belongsToMany(Product, {
-  
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
